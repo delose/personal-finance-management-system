d86d8b4c617311f2a396414a46b2c0bdb5b09d25
delose <delossantos.eugene@gmail.com>
2024-09-09 09:49:12 +0800

    Initial commit

---
+++ src/main/java/com/delose/pfms/PfmsApplication.java
@@ -0,0 +1,13 @@
+package com.delose.pfms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PfmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PfmsApplication.class, args);
+	}
+
+}
