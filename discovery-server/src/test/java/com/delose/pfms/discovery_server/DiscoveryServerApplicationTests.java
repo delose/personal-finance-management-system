e8c1c73b017d9edf13cfd43c433d969275f19009
delose <delossantos.eugene@gmail.com>
2024-09-13 08:20:19 +0800

    discover server tested manually - includes integration test to check if eureka is up

--- discovery-server/src/test/java/com/delose/pfms/discovery_server/DiscoveryServerApplicationTests.java
+++ discovery-server/src/test/java/com/delose/pfms/discovery_server/DiscoveryServerApplicationTests.java
@@ -2,12 +2,26 @@ package com.delose.pfms.discovery_server;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 class DiscoveryServerApplicationTests {
+    @LocalServerPort
+    private int port;
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+        // This test checks if the application context loads correctly.
+    }
 
+    @Test
+    void eurekaServerIsUp() {
+        String url = "http://localhost:" + port + "/eureka/apps";
+        RestTemplate restTemplate = new RestTemplate();
+        String response = restTemplate.getForObject(url, String.class);
+        assertThat(response).contains("applications");
+    }
 }
