4fa0ef2ae0a1a049bb29586fc01ace900e819377
feathrsaas <founder@feathrs.com>
2025-10-04 18:22:13 +0800

    init budget pojo

---
+++ budget-service/src/main/java/com/delose/pfms/budget_service/entity/Budget.java
@@ -0,0 +1,18 @@
+package com.delose.pfms.budget_service.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "budgets")
+public class Budget {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    
+}
