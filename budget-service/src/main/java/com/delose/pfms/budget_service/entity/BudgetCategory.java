1b85e6a5137087ff0fe2d49d3e2f3f4eac28bd2d
delose <delossantos.eugene@gmail.com>
2025-11-27 09:40:16 +0800

    BudgetCategory enum

---
+++ budget-service/src/main/java/com/delose/pfms/budget_service/entity/BudgetCategory.java
@@ -0,0 +1,13 @@
+package com.delose.pfms.budget_service.entity;
+
+public enum BudgetCategory {
+    GROCERIES,
+    UTILITIES,
+    RENT,
+    TRAVEL,
+    FOOD,
+    ENTERTAINMENT,
+    DINING,
+    SHOPPING,
+    OTHER
+}
