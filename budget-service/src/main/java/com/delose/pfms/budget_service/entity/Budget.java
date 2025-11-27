4428340c0c5690fbed8f8a001d73b17a79ae0fc7
delose <delossantos.eugene@gmail.com>
2025-11-27 08:58:51 +0800

    Update Budget entity

--- budget-service/src/main/java/com/delose/pfms/budget_service/entity/Budget.java
+++ budget-service/src/main/java/com/delose/pfms/budget_service/entity/Budget.java
@@ -5,14 +5,26 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 @Entity
 @Table(name = "budgets")
+@Data
 public class Budget {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    private String category;
+
+    private BigDecimal amount;
+
+    private LocalDate startDate;
+
+    private LocalDate endDate;
     
 }
