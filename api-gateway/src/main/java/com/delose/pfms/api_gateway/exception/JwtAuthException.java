95266f90f504f102b859675f3d50493241d31c39
delose <delossantos.eugene@gmail.com>
2025-12-14 22:24:43 +0800

    rm JwtAuthException

--- api-gateway/src/main/java/com/delose/pfms/api_gateway/exception/JwtAuthException.java
+++
@@ -1,7 +0,0 @@
-package com.delose.pfms.api_gateway.exception;
-
-public class JwtAuthException extends RuntimeException {
-    public JwtAuthException(String message) {
-        super(message);
-    }
-}
