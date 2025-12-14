04c329f4131c2af5236d8228b48e17391788dd07
delose <delossantos.eugene@gmail.com>
2025-12-14 22:30:19 +0800

    init UserRepository

---
+++ api-gateway/src/main/java/com/delose/pfms/api_gateway/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.delose.pfms.api_gateway.repository;
+
+import com.delose.pfms.api_gateway.entity.User;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    Optional<User> findByEmail(String email);
+}
