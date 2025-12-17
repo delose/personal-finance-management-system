251aacc0fbb4f764255aa94aa580b4e2552a47dc
delose <delossantos.eugene@gmail.com>
2025-12-17 14:14:21 +0800

    move indent back [fix]

--- api-gateway/run-docker.sh
+++ api-gateway/run-docker.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 docker run -d -p 8080:8080 --name api-gateway delose/api-gateway:latest \
-  -e SPRING_DATASOURCE_URL=jdbc:mysql://host.docker.internal:3307/apigwdb?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false \
+  -e SPRING_DATASOURCE_URL='jdbc:mysql://host.docker.internal:3307/apigwdb?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false' \
   -e SPRING_DATASOURCE_USERNAME=root \
   -e SPRING_DATASOURCE_PASSWORD=secret
 
