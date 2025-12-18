ab33a9279460af472ee9c527e22221c921f6aa33
delose <delossantos.eugene@gmail.com>
2025-12-18 08:29:56 +0800

    Resolve host.docker.internal correctly

--- api-gateway/run-docker.sh
+++ api-gateway/run-docker.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
-docker run -d -p 8080:8080 --name api-gateway delose/api-gateway:latest \
-  -e SPRING_DATASOURCE_URL='jdbc:mysql://host.docker.internal:3307/apigwdb?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false' \
-  -e SPRING_DATASOURCE_USERNAME=root \
-  -e SPRING_DATASOURCE_PASSWORD=secret
+# 1. Stop and remove existing container
+docker stop api-gateway 2>/dev/null
+docker rm api-gateway 2>/dev/null
 
+# 2. Run the container
+# We use --add-host to ensure host.docker.internal resolves correctly
+# We use SPRING_PROFILES_ACTIVE=docker to trigger application-docker.properties
+docker run -d -p 8080:8080 --name api-gateway \
+  --add-host=host.docker.internal:host-gateway \
+  -e SPRING_PROFILES_ACTIVE=docker \
+  -e SPRING_DATASOURCE_URL='jdbc:mysql://host.docker.internal:3307/apigwdb?serverTimezone=UTC&allowPublicKeyRetrieval=true&useSSL=false' \
+  -e SPRING_DATASOURCE_USERNAME='root' \
+  -e SPRING_DATASOURCE_PASSWORD='secret' \
+  delose/api-gateway:latest
