718298e92bd25f60a0463bfe7ad81255a6de9024
delose <delossantos.eugene@gmail.com>
2025-12-21 16:55:47 +0800

    added health endpoint with User

--- user-service/src/main/java/com/delose/pfms/user_service/controller/UserController.java
+++ user-service/src/main/java/com/delose/pfms/user_service/controller/UserController.java
@@ -1,5 +1,7 @@
 package com.delose.pfms.user_service.controller;
 
+import com.delose.pfms.user_service.service.HealthService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import com.delose.pfms.user_service.entity.User;
@@ -7,12 +9,24 @@ import com.delose.pfms.user_service.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/api/v1/users")
-@RequiredArgsConstructor
 public class UserController {
 
     private final UserService userService;
+    private final HealthService healthService;
+
+    public UserController(UserService userService, HealthService healthService) {
+        this.userService = userService;
+        this.healthService = healthService;
+    }
+
+    @GetMapping("/health")
+    public ResponseEntity<Map<String, Object>> getHealthInfo() {
+        return this.healthService.getContainerInfo();
+    }
 
     @GetMapping("/{username}")
     public User getUserByUsername(@PathVariable("username") String username) {
