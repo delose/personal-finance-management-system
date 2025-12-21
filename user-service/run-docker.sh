6ba8b851c384593daeac35997712427a303feafd
delose <delossantos.eugene@gmail.com>
2025-12-21 16:57:11 +0800

    unreachable URL [FIX]

--- user-service/run-docker.sh
+++ user-service/run-docker.sh
@@ -6,7 +6,7 @@ docker rm user-service 2>/dev/null
 # •	<host_port> is the port on your host machine (i.e., your MacBook) that you want to map.
 # •	<container_port> is the port that the application is listening on inside the Docker container.
 
-docker run -d -p 8081:8080 --name user-service \
+docker run -d -p 8081:8081 --name user-service \
   --add-host=host.docker.internal:host-gateway \
   -e SPRING_PROFILES_ACTIVE=docker \
   delose/user-service:latest
