d48e69182981c8aaf89380d9e5339ef35c264ca4
delose <delossantos.eugene@gmail.com>
2025-12-21 13:15:08 +0800

    run script for config server

--- config-server/run-docker.sh
+++ config-server/run-docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-docker run -d -p 8888:8888 --name config-server delose/config-server:latest
+docker stop config-server 2>/dev/null
+docker rm config-server 2>/dev/null
+
+docker run -d -p 8888:8888 --name config-server \
+  --add-host=host.docker.internal:host-gateway \
+  -e SPRING_PROFILES_ACTIVE=docker \
+  delose/config-server:latest
 
 
