0b1e33f82c6de95a86ea316cc737798ebf727d68
delose <delossantos.eugene@gmail.com>
2025-12-26 08:57:30 +0800

    from forbidden to OK

--- goal-service/src/main/java/com/delose/pfms/goal_service/controller/HealthController.java
+++ goal-service/src/main/java/com/delose/pfms/goal_service/controller/HealthController.java
@@ -11,6 +11,6 @@ public class HealthController {
     @GetMapping("/goal-health-check")
     public ResponseEntity<String> myCustomCheck() {
         String message = "Testing my healh check function";
-        return new ResponseEntity<>(message, HttpStatus.FORBIDDEN);
+        return new ResponseEntity<>(message, HttpStatus.OK);
     }
 }
