a4a248bc26714275f2500fee795074499103dad0
delose <delossantos.eugene@gmail.com>
2025-12-30 19:57:56 +0800

    notif service > README > run-docker

--- notification-service/run-docker.sh
+++ notification-service/run-docker.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+docker stop notification-service 2>/dev/null
+docker rm notification-service 2>/dev/null
 # •	-p <host_port>:<container_port>:
 # •	<host_port> is the port on your host machine (i.e., your MacBook) that you want to map.
 # •	<container_port> is the port that the application is listening on inside the Docker container.
 
-docker run -d -p 8085:8080 --name notification-service delose/notification-service:latest
+docker run -d -p 8085:8080 --name notification-service \
+  --add-host=host.docker.internal:host-gateway \
+  -e SPRING_PROFILES_ACTIVE=docker \
+  -e CONSUL_HOST=host.docker.internal \
+  -e CONSUL_PORT=8500 \
+  delose/notification-service:latest
 
 
