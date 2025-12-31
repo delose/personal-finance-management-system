53a7141fb8d7315dfe4ebd64291fedee1a96f224
delose <delossantos.eugene@gmail.com>
2025-12-31 08:56:55 +0800

    discovery server run docker sh file

--- discovery-server/run-docker.sh
+++ discovery-server/run-docker.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
-docker run -d -p 8761:8761 --name discovery-server delose/discovery-server:latest
+docker stop discovery-server 2>/dev/null
+docker rm discovery-server 2>/dev/null
+
+docker run -d -p 8761:8761 --name discovery-server \
+  --add-host=host.docker.internal:host-gateway \
+  -e SPRING_PROFILES_ACTIVE=docker \
+  delose/discovery-server:latest
 
 
