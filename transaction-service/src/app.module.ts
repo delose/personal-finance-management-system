0ee2e51376bfd2a9935420d350d5c3cd658435f5
delose <delossantos.eugene@gmail.com>
2026-01-01 14:02:06 +0800

    transaction connectivity RMQ for account service

--- transaction-service/src/app.module.ts
+++ transaction-service/src/app.module.ts
@@ -1,9 +1,25 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ClientsModule, Transport } from '@nestjs/microservices';
+import { ACCOUNT_SERVICE } from './constants';
 
 @Module({
-  imports: [],
+  imports: [
+        ClientsModule.register([
+          {
+            name: ACCOUNT_SERVICE,
+            transport: Transport.RMQ,
+            options: {
+              urls: ['amqp://guest:guest@localhost:5672'],
+              queue: 'account_queue',
+              queueOptions: {
+                durable: true
+              },
+            },
+          },
+        ]),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
