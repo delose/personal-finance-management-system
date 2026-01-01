d3a3a0e55b671e367013e169f6ca5c6c33be0e87
delose <delossantos.eugene@gmail.com>
2026-01-01 14:01:06 +0800

    create account endpoint - see README to test

--- transaction-service/src/app.controller.ts
+++ transaction-service/src/app.controller.ts
@@ -1,12 +1,28 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Inject, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ACCOUNT_SERVICE } from './constants';
+import { ClientProxy, MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService,
+              @Inject(ACCOUNT_SERVICE) private readonly accountRMQClient: ClientProxy
+    ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
+
+  @Post('account')
+  createAccount(@Body() account: any) {
+    this.accountRMQClient.emit("account-created", account);
+    let MESSAGE = "Account creation request sent to RabbitMq: " + JSON.stringify(account);
+    console.log(MESSAGE);
+    return { message: MESSAGE + ": ", account };
+  }
+
+  // Transactions acts as gateway for actual CRUD operations for
+  // 1. account
+  // n. others
 }
