e6765dc12d7cf1a91dc66b89510b22ed19f6250e
delose <delossantos.eugene@gmail.com>
2026-01-01 14:23:27 +0800

    account service > removed injected appService

--- account-service/src/app.controller.ts
+++ account-service/src/app.controller.ts
@@ -1,18 +1,11 @@
 import { Controller, Get, Post } from '@nestjs/common';
-import { AppService } from './app.service';
 import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
-  }
 
   @MessagePattern("account-created")
   handleAccountCreated(@Payload() account: any) {
-    console.log('[Account-Service]: Received account: ', account);
+    console.log('[Account-Service: Received account: ', account);
   }
 }
