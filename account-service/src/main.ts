fe578098ae8d664b179380a87163cd9f198cbc57
delose <delossantos.eugene@gmail.com>
2026-01-01 14:04:01 +0800

    account service connection with RMQ

--- account-service/src/main.ts
+++ account-service/src/main.ts
@@ -1,8 +1,24 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { ACCOUNT_SERVICE } from './constants';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const app = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+        name: ACCOUNT_SERVICE,
+        transport: Transport.RMQ,
+        options: {
+          urls: ['amqp://guest:guest@localhost:5672'],
+          queue: 'account_queue',
+          queueOptions: {
+            durable: true
+          },
+        },
+    }
+  );
+  await app.listen();
+  console.log(`Application is listening to RabbitMQ...`);
 }
 bootstrap();
