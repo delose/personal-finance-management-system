6bf198912f1e27d10db87134a62df341147ddabf
delose <delossantos.eugene@gmail.com>
2026-01-02 15:10:17 +0800

    force NestJS to wait for message to be dispatched [fix]

--- transaction-service/src/app.controller.ts
+++ transaction-service/src/app.controller.ts
@@ -2,12 +2,17 @@ import { Controller, Get, Inject, Post, Body } from '@nestjs/common';
 import { AppService } from './app.service';
 import { ACCOUNT_SERVICE } from './constants';
 import { ClientProxy, MessagePattern, Payload } from '@nestjs/microservices';
+import { lastValueFrom } from 'rxjs';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService,
               @Inject(ACCOUNT_SERVICE) private readonly accountRMQClient: ClientProxy
-    ) {}
+    ) {
+      this.accountRMQClient.status.subscribe(status => {
+        console.log('RMQ Client Status:', status); // Should log 'connected'
+      });
+    }
 
   @Get('health')
   checkHealth() {
@@ -19,13 +24,16 @@ export class AppController {
     return this.appService.getHello();
   }
 
-  @Post('account')
-  createAccount(@Body() account: any) {
-    this.accountRMQClient.emit("account-created", account);
-    let MESSAGE = "Account creation request sent to RabbitMq: " + JSON.stringify(account);
-    console.log(MESSAGE);
-    return { message: MESSAGE + ": ", account };
-  }
+    @Post('account')
+    async createAccount(@Body() account: any) {
+      try {
+        // This forces NestJS to wait for the message to be dispatched
+        await lastValueFrom(this.accountRMQClient.emit("account-created", account));
+        console.log("Account emitted successfully:", account);
+      } catch (err) {
+        console.error("Failed to emit to RMQ:", err);
+      }
+    }
 
   // Transactions acts as gateway for actual CRUD operations for
   // 1. account
