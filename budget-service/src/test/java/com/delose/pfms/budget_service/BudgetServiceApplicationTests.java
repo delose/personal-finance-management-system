116d6fe66cdd8ac307741a54f916b0d52d53bc56
delose <delossantos.eugene@gmail.com>
2026-01-05 14:29:12 +0800

    budget service > integration test to use embedded kafka + dev active profile

--- budget-service/src/test/java/com/delose/pfms/budget_service/BudgetServiceApplicationTests.java
+++ budget-service/src/test/java/com/delose/pfms/budget_service/BudgetServiceApplicationTests.java
@@ -2,8 +2,12 @@ package com.delose.pfms.budget_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@EmbeddedKafka
+@ActiveProfiles("dev")
 class BudgetServiceApplicationTests {
 
 	@Test
