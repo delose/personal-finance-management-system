60b49f5c1ed52faf678ee70f67c29c7105089c08
delose <delossantos.eugene@gmail.com>
2026-01-05 16:33:46 +0800

    notification service > reflect the notification request with the same fields as budget notification [fix]

--- notification-service/src/main/java/com/delose/pfms/notification_service/dto/NotificationRequest.java
+++ notification-service/src/main/java/com/delose/pfms/notification_service/dto/NotificationRequest.java
@@ -1,11 +1,30 @@
 package com.delose.pfms.notification_service.dto;
 
 public class NotificationRequest {
+    private Long id;
     private String userId;
-    private String recipient;
     private String subject;
     private String messageBody;
     private String channel;
+
+    public NotificationRequest() {
+    }
+
+    public NotificationRequest(Long id, String userId, String subject, String messageBody, String channel) {
+        this.id = id;
+        this.userId = userId;
+        this.subject = subject;
+        this.messageBody = messageBody;
+        this.channel = channel;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 
     public String getUserId() {
         return userId;
@@ -13,14 +32,6 @@ public class NotificationRequest {
 
     public void setUserId(String userId) {
         this.userId = userId;
-    }
-
-    public String getRecipient() {
-        return recipient;
-    }
-
-    public void setRecipient(String recipient) {
-        this.recipient = recipient;
     }
 
     public String getSubject() {
@@ -45,27 +56,5 @@ public class NotificationRequest {
 
     public void setChannel(String channel) {
         this.channel = channel;
-    }
-
-    public NotificationRequest(String userId, String recipient, String subject, String messageBody, String channel) {
-        this.userId = userId;
-        this.recipient = recipient;
-        this.subject = subject;
-        this.messageBody = messageBody;
-        this.channel = channel;
-    }
-
-    public NotificationRequest() {
-    }
-
-    @Override
-    public String toString() {
-        return "NotificationRequest{" +
-                "userId='" + userId + '\'' +
-                ", recipient='" + recipient + '\'' +
-                ", subject='" + subject + '\'' +
-                ", messageBody='" + messageBody + '\'' +
-                ", channel='" + channel + '\'' +
-                '}';
     }
 }
