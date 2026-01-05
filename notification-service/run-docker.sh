554ab37e54dae59d871577b69f792b5d72987151
delose <delossantos.eugene@gmail.com>
2026-01-05 14:38:40 +0800

    notification service > run docker debug in progress

--- notification-service/run-docker.sh
+++ notification-service/run-docker.sh
@@ -11,6 +11,8 @@ docker run -d -p 8085:8085 --name notification-service \
   -e SPRING_PROFILES_ACTIVE=docker \
   -e CONSUL_HOST=host.docker.internal \
   -e CONSUL_PORT=8500 \
+  -e spring.kafka.bootstrap-servers="global-service-kafka:29092" \
+  -e spring.kafka.producer.value-serializer="org.springframework.kafka.support.serializer.JsonSerializer" \
   delose/notification-service:latest
 
 
