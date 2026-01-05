974aec951c97f74e49cdbbb332ca0115b40cbc79
delose <delossantos.eugene@gmail.com>
2026-01-05 14:22:54 +0800

    budget service > apply transactional, mapper and producer [debugging in progress]

--- budget-service/src/main/java/com/delose/pfms/budget_service/service/BudgetService.java
+++ budget-service/src/main/java/com/delose/pfms/budget_service/service/BudgetService.java
@@ -1,12 +1,16 @@
 package com.delose.pfms.budget_service.service;
 
+import com.delose.pfms.budget_service.dto.BudgetNotification;
 import com.delose.pfms.budget_service.entity.Budget;
+import com.delose.pfms.budget_service.mapper.BudgetMapper;
 import com.delose.pfms.budget_service.repository.BudgetRepository;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cache.annotation.Cacheable;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 import java.util.Optional;
@@ -15,16 +19,28 @@ import java.util.Optional;
 public class BudgetService {
     private static final Logger log = LoggerFactory.getLogger(BudgetService.class);
 
-
     @Autowired
     private BudgetRepository budgetRepository;
+
+    @Autowired
+    private BudgetMapper budgetMapper;
+
+    @Autowired
+    private ApplicationEventPublisher eventPublisher;
 
     public List<Budget> findAllBudgets() {
         return this.budgetRepository.findAll();
     }
 
+    @Transactional
     public Budget saveBudget(Budget budget) {
-        return this.budgetRepository.save(budget);
+        Budget savedBudget = this.budgetRepository.save(budget);
+
+        BudgetNotification notification = budgetMapper.toNotification(savedBudget);
+
+        eventPublisher.publishEvent(notification);
+
+        return savedBudget;
     }
 
     @Cacheable(value = "budgets")
