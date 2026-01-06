34bce7dad35f78c209da11a1434ed9374d6ac82e
delose <delossantos.eugene@gmail.com>
2026-01-06 09:47:42 +0800

    notification service > use ErrorHandlingDeserializer; define delegate class and json value default type; use relaxed config [kafka fix]

--- notification-service/run-docker.sh
+++ notification-service/run-docker.sh
@@ -7,12 +7,16 @@ docker rm notification-service 2>/dev/null
 # •	<container_port> is the port that the application is listening on inside the Docker container.
 
 docker run -d -p 8085:8085 --name notification-service \
+  --network pfms-network \
   --add-host=host.docker.internal:host-gateway \
   -e SPRING_PROFILES_ACTIVE=docker \
   -e CONSUL_HOST=host.docker.internal \
   -e CONSUL_PORT=8500 \
-  -e spring.kafka.bootstrap-servers="global-service-kafka:29092" \
-  -e spring.kafka.producer.value-serializer="org.springframework.kafka.support.serializer.JsonSerializer" \
+  -e SPRING_KAFKA_CONSUMER_VALUE_DESERIALIZER="org.springframework.kafka.support.serializer.ErrorHandlingDeserializer" \
+  -e SPRING_KAFKA_CONSUMER_PROPERTIES_SPRING_DESERIALIZER_VALUE_DELEGATE_CLASS="org.springframework.kafka.support.serializer.JsonDeserializer" \
+  -e SPRING_KAFKA_CONSUMER_PROPERTIES_SPRING_JSON_TRUSTED_PACKAGES="*" \
+  -e SPRING_KAFKA_CONSUMER_PROPERTIES_SPRING_JSON_USE_TYPE_HEADERS="false" \
+  -e SPRING_KAFKA_CONSUMER_PROPERTIES_SPRING_JSON_VALUE_DEFAULT_TYPE="com.delose.pfms.notification_service.dto.NotificationRequest" \
   delose/notification-service:latest
 
 
