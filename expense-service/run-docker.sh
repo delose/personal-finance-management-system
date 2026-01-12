e37686f8ef93bca8b0d19d87422f37c42c0368a5
delose <delossantos.eugene@gmail.com>
2026-01-13 07:48:31 +0800

    expense-service PHP impl > from docker run to docker compose

--- expense-service/run-docker.sh
+++ expense-service/run-docker.sh
@@ -1,16 +1,3 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-docker stop expense-service 2>/dev/null
-docker rm expense-service 2>/dev/null
-# •	-p <host_port>:<container_port>:
-# •	<host_port> is the port on your host machine (i.e., your MacBook) that you want to map.
-# •	<container_port> is the port that the application is listening on inside the Docker container.
-
-docker run -d -p 8083:8083 --name expense-service \
-  --add-host=host.docker.internal:host-gateway \
-  -e SPRING_PROFILES_ACTIVE=docker \
-  -e CONSUL_HOST=host.docker.internal \
-  -e CONSUL_PORT=8500 \
-  delose/expense-service:latest
-
-
+docker compose up -d
