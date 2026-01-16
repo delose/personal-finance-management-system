80a178c284c9ca835130ccfa0235deb26b24668b
delose <delossantos.eugene@gmail.com>
2026-01-16 10:03:38 +0800

    user-service Go impl > nuke HealthService.java, User.java, UserController.java, UserControllerTest.java, UserException.java, UserRepository.java, UserService.java, UserServiceApplication.java, UserServiceApplicationTests.java, UserServiceTest.java

--- user-service/src/main/java/com/delose/pfms/user_service/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.delose.pfms.user_service.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.delose.pfms.user_service.entity.User;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    User findByUsername(String username);
-}
