033e4a0ee2bcc788f8cb86e11581cab4943b6df1
delose <delossantos.eugene@gmail.com>
2026-01-23 07:33:47 +0800

    analytics service > always rebuild during verify [fix]

--- analytics-service/verify-build.sh
+++ analytics-service/verify-build.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/env bash
-# Check if the image 'pfms-analytics' (or whatever your image name is) exists
-if [ -n "$(docker images -q analytics-service-analytics-service)" ]; then
-  exit 0 # Image exists, skip build
-else
-  exit 1 # Image missing, need build
-fi
+docker-compose down
+
+exit 1
