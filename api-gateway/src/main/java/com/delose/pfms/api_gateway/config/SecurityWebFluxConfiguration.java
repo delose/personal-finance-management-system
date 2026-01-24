48e91595a8acf7d62601b89af7898685fe74389f
delose <delossantos.eugene@gmail.com>
2026-01-24 20:02:33 +0800

    apigw > fix for CORS

--- api-gateway/src/main/java/com/delose/pfms/api_gateway/config/SecurityWebFluxConfiguration.java
+++ api-gateway/src/main/java/com/delose/pfms/api_gateway/config/SecurityWebFluxConfiguration.java
@@ -11,8 +11,11 @@ import org.springframework.security.web.server.authentication.AuthenticationWebF
 import org.springframework.security.web.server.authentication.HttpStatusServerEntryPoint;
 import org.springframework.security.web.server.context.NoOpServerSecurityContextRepository;
 import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
 import org.springframework.web.cors.reactive.CorsWebFilter;
 import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.List;
 
 @Configuration
 @EnableWebFluxSecurity
