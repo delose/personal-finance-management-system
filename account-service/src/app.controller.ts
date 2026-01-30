f6c4962c0e87fcce10ae6646e06bae9d96153a29
delose <delossantos.eugene@gmail.com>
2026-01-30 08:22:06 +0800

    account service > GET endpoint for health checks

--- account-service/src/app.controller.ts
+++ account-service/src/app.controller.ts
@@ -4,6 +4,15 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 @Controller()
 export class AppController {
 
+  @Get('health')
+
+  health(): string {
+
+    return 'OK';
+
+  }
+
+
   @MessagePattern("account-created")
   handleAccountCreated(@Payload() account: any) {
     console.log('[Account-Service: Received account: ', account);
