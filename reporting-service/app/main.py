f1733311351d8d27a88026b63de67e1a62dea133
delose <delossantos.eugene@gmail.com>
2026-02-04 11:55:13 +0800

    reporting service main.py > fix for registering to consul

--- reporting-service/app/main.py
+++ reporting-service/app/main.py
@@ -3,6 +3,7 @@ import logging
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import consul
+import socket
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -24,15 +25,9 @@ async def lifespan(app: FastAPI):
     try:
         logger.info(f"Registering {SERVICE_NAME} with Consul at {CONSUL_HOST}:{CONSUL_PORT}")
         
-        # Use the container's hostname for the address
-        address = os.getenv('HOSTNAME', 'localhost')
-        
-        # Construct the health check URL. 
-        # Note: Consul needs to be able to reach this address. 
-        # If Consul is in the same network, using the container IP or hostname works.
-        # We use the address variable here to ensure consistency.
+        address = os.getenv('SERVICE_IP', socket.gethostbyname(socket.gethostname()))
         check_url = f"http://{address}:{SERVICE_PORT}/health"
-        
+
         consul_client.agent.service.register(
             name=SERVICE_NAME,
             service_id=SERVICE_ID,
