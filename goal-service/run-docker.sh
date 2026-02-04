4950ae4d86d5129b55486e6db7ccd90f04b479d6
delose <delossantos.eugene@gmail.com>
2026-02-04 13:03:09 +0800

    goal service > update run docker to run docker compose

--- goal-service/run-docker.sh
+++ goal-service/run-docker.sh
@@ -1,17 +1,19 @@
 #!/bin/bash
 
-docker stop goal-service 2>/dev/null
-docker rm goal-service 2>/dev/null
+#docker stop goal-service 2>/dev/null
+#docker rm goal-service 2>/dev/null
 
 # •	-p <host_port>:<container_port>:
 # •	<host_port> is the port on your host machine (i.e., your MacBook) that you want to map.
 # •	<container_port> is the port that the application is listening on inside the Docker container.
 
-docker run -d -p 8084:8084 --name goal-service \
-  --add-host=host.docker.internal:host-gateway \
-  -e SPRING_PROFILES_ACTIVE=docker \
-  -e CONSUL_HOST=host.docker.internal \
-  -e CONSUL_PORT=8500 \
-  delose/goal-service:latest
+#docker run -d -p 8084:8084 --name goal-service \
+#  --add-host=host.docker.internal:host-gateway \
+#  -e SPRING_PROFILES_ACTIVE=docker \
+#  -e CONSUL_HOST=host.docker.internal \
+#  -e CONSUL_PORT=8500 \
+#  delose/goal-service:latest
+
+./compose-run.sh
 
 
